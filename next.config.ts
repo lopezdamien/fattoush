8d27a01f19434a86dffc80473c8c99872910a40e
lopezdamien <damienlopez@outlook.fr>
2026-02-08 21:28:05 +0100

    Fix next.config.ts type error

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
+import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
     output: 'standalone',
 };
 
