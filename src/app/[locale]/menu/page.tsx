fd8026447c4ab6ab359f267b4691010fe1f4913f
lopezdamien <damienlopez@outlook.fr>
2026-03-09 23:19:47 +0100

    Update plats du jour to plural

--- src/app/[locale]/menu/page.tsx
+++ src/app/[locale]/menu/page.tsx
@@ -142,7 +142,7 @@ export default function MenuPage() {
 
                         <div className="relative z-10 inline-flex items-center space-x-3 justify-center w-full">
                             <span className="text-xs md:text-sm font-bold tracking-[0.2em] text-primary uppercase">
-                                Plat du jour • Mardi 10 Mars
+                                Plats du jour • Mardi 10 Mars
                             </span>
                         </div>
 
