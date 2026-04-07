27bebd8ec50d9d97d53b2568bf6e20c85e75d454
lopezdamien <damienlopez@outlook.fr>
2026-04-08 01:27:38 +0200

    update plat du jour to ouzi d'agneau

--- src/app/[locale]/menu/page.tsx
+++ src/app/[locale]/menu/page.tsx
@@ -142,19 +142,19 @@ export default function MenuPage() {
 
                         <div className="relative z-10 inline-flex items-center space-x-3 justify-center w-full">
                             <span className="text-xs md:text-sm font-bold tracking-[0.2em] text-primary uppercase text-center">
-                                Plat du jour • Mardi 7 Avril
+                                Plat du jour • Mercredi 8 Avril
                             </span>
                         </div>
 
                         <div className="relative z-10 grid grid-cols-1 lg:grid-cols-2 gap-8 w-full max-w-5xl text-center mt-2">
                             <div className="flex flex-col items-center space-y-2">
                                 <h2 className="text-xl md:text-2xl font-bold text-foreground uppercase">
-                                    AUBERGINES FARCIES
+                                    OUZI D’AGNEAU
                                 </h2>
                                 <p className="text-sm md:text-base text-foreground/80 font-medium">
-                                    À la sauce béchamel, accompagnées de riz
+                                    Riz et agneau en pâte feuilletée
                                     <br />
-                                    Servies avec une salade fraîche
+                                    Servi avec yogourt au concombre
                                 </p>
                                 <span className="text-lg font-bold text-primary mt-2">
                                     CHF 23.–
